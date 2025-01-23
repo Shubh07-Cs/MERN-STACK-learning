dc86e3fffc1be17441aa321c9d5ca666990e0aa3
Shubh07-Cs <srai21029@gmail.com>
2025-01-23 16:04:45 +0530

    Day 9 and assignment

--- Day-8/DOM_basics.js
+++ Day-8/DOM_basics.js
@@ -1,9 +1,18 @@
-window.console.log("hello");
-window.console.log(window);
-window.console.log(document);
+// window.console.log("hello");
+// window.console.log(window);
+// window.console.log(document);
 
-document.body.children[0].innerText;
-console.log(document.body.children[0].innerText);
-// DOM Manipulation :: Changing something in DOM using Javascript (document keyword : DOM API)
-document.body.children[0].innerText = "Done for the first half!";
-console.log(document.body.children[0].innerText);
+// document.body.children[0].innerText;
+// console.log(document.body.children[0].innerText);
+// // DOM Manipulation :: Changing something in DOM using Javascript (document keyword : DOM API)
+// document.body.children[0].innerText = "Done for the first half!";//not ideal methd. To lengthy and messy
+// console.log(document.body.children[0].innerText);
+
+//-------------------------
+//Dom Methods
+//1. getElementbyTagName
+const titles= window.document.getElementsByTagName("h3");
+console.log(titles)
+
+titles[0].style.backgroundColor="yellow";
+titles[1].style.backgroundColor="red";
---
+++ Day-9/events.js
@@ -0,0 +1,11 @@
+function handleDivClick(){
+    console.log("Div clicked");
+}
+
+function handleMouseLeave(){
+    console.log("Mouse clicked");
+}
+
+function handleMouseEnter(){
+    console.log("Mouse Entered");
+}
