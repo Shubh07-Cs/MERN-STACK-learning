d4ad8c2e98ef19a952be985f650f10c607fe8662
Shubh07-Cs <srai21029@gmail.com>
2025-01-31 11:17:37 +0530

    node js basic and export import of modules

---
+++ Day-16 node js/teamA.js
@@ -0,0 +1,3 @@
+console.log("Hello");
+const add=require("./teamB")
+console.log(add(2,3));
---
+++ Day-16 node js/teamB.js
@@ -0,0 +1,7 @@
+
+
+function sum( a, b){
+    console.log("Sum is: ");
+    return a+b;
+}
+module.exports=sum;
