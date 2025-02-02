df17ac6381b3bd04b93bfebd26ef19073212a34b
Shubh07-Cs <srai21029@gmail.com>
2025-02-02 14:06:19 +0530

    react very very vasic and mostly theory

---
+++ React Practice/reactlvl0.js
@@ -0,0 +1,9 @@
+console.log(React);
+console.log(ReactDOM);
+
+
+//Crreating root and connecting it to react.
+const domElement = document.getElementById("dom-root");
+const root=ReactDOM.createRoot(domElement);
+
+console.log(root);
